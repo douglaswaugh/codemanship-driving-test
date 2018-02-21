cf6c954c6e54b4b7f4bf0804aa371b596b45f415
Douglas Waugh <douglas.waugh@energyhelpline.com>
2018-02-21 16:13:45 +0000

    Exclude base programmer and correct degrees of separation in network

--- src/pronet/DegreesOfSeparationNetwork.cs
+++ src/pronet/DegreesOfSeparationNetwork.cs
@@ -9,7 +9,7 @@ namespace ProNet
         public List<Tuple<int, IProgrammer>> BuildNetwork(IProgrammer programmerFrom)
         {
             var toProcess = new Queue<Tuple<int, IProgrammer>>();
-            AddRelationsTo(toProcess, 2, programmerFrom, new List<Tuple<int, IProgrammer>>());
+            AddRelationsTo(programmerFrom, toProcess, 1, programmerFrom, new List<Tuple<int, IProgrammer>>());
 
             var network = new List<Tuple<int, IProgrammer>>();
 
@@ -19,17 +19,17 @@ namespace ProNet
 
                 network.Add(programmerToProcess);
 
-                AddRelationsTo(toProcess, programmerToProcess.Item1 + 1, programmerToProcess.Item2, network);
+                AddRelationsTo(programmerFrom, toProcess, programmerToProcess.Item1 + 1, programmerToProcess.Item2, network);
             }
 
             return network;
         }
 
-        public void AddRelationsTo(Queue<Tuple<int, IProgrammer>> queue, int degreeOfSeparation, IProgrammer processed, List<Tuple<int, IProgrammer>> network)
+        public void AddRelationsTo(IProgrammer programmerFrom, Queue<Tuple<int, IProgrammer>> queue, int degreeOfSeparation, IProgrammer processed, List<Tuple<int, IProgrammer>> network)
         {
             foreach (var relation in processed.Relations)
             {
-                if (processed != relation && !queue.Any(tuple => tuple.Item2.Name == relation.Name) && !network.Any(tuple => tuple.Item2.Name == relation.Name))
+                if (processed != relation && programmerFrom != relation && !queue.Any(tuple => tuple.Item2.Name == relation.Name) && !network.Any(tuple => tuple.Item2.Name == relation.Name))
                 {
                     queue.Enqueue(new Tuple<int, IProgrammer>(degreeOfSeparation, relation));
                 }
--- tests/pronet-tests/DegreesOfSeparationNetworkTests.cs
+++ tests/pronet-tests/DegreesOfSeparationNetworkTests.cs
@@ -19,7 +19,7 @@ namespace Tests
 
             programmer1.Recommends(programmer2);
 
-            degreesOfSeparation.AddRelationsTo(queue, 1, programmer1, new List<Tuple<int, IProgrammer>>());
+            degreesOfSeparation.AddRelationsTo(programmer1, queue, 1, programmer1, new List<Tuple<int, IProgrammer>>());
 
             var expectedQueue = new Queue<Tuple<int, IProgrammer>>();
             expectedQueue.Enqueue(new Tuple<int, IProgrammer>(1, programmer2));
@@ -37,7 +37,7 @@ namespace Tests
             var degreesOfSeparation = new DegreesOfSeparationNetwork();
             var queue = new Queue<Tuple<int, IProgrammer>>();
 
-            degreesOfSeparation.AddRelationsTo(queue, 1, programmer2, new List<Tuple<int, IProgrammer>>());
+            degreesOfSeparation.AddRelationsTo(programmer2, queue, 1, programmer2, new List<Tuple<int, IProgrammer>>());
 
             var expectedQueue = new Queue<Tuple<int, IProgrammer>>();
             expectedQueue.Enqueue(new Tuple<int, IProgrammer>(1, programmer1));
@@ -59,11 +59,30 @@ namespace Tests
             queue.Enqueue(new Tuple<int, IProgrammer>(1, programmerAlreadyInQueue));
 
             var degreesOfSeparation = new DegreesOfSeparationNetwork();
-            degreesOfSeparation.AddRelationsTo(queue, 2, programmer1 , new List<Tuple<int, IProgrammer>>());
+            degreesOfSeparation.AddRelationsTo(programmer1, queue, 2, programmer1 , new List<Tuple<int, IProgrammer>>());
 
             var expectedQueue = new Queue<Tuple<int, IProgrammer>>();
             expectedQueue.Enqueue(new Tuple<int, IProgrammer>(1, programmerAlreadyInQueue));
             expectedQueue.Enqueue(new Tuple<int, IProgrammer>(2, programmer2));
+
+            Assert.That(queue, Is.EquivalentTo(expectedQueue));
+        }
+
+        [Test]
+        public void Should_build_network_with_one_degree_of_separation()
+        {
+            var programmer1 = new Programmer("Programmer1", new string[]{});
+            var programmer2 = new Programmer("Programmer2", new string[]{});
+            var programmer3 = new Programmer("Programmer3", new string[]{});
+            programmer1.Recommends(programmer2);
+            programmer1.Recommends(programmer3);
+
+            var degreesOfSeparation = new DegreesOfSeparationNetwork();
+            var queue = degreesOfSeparation.BuildNetwork(programmer1);
+
+            var expectedQueue = new Queue<Tuple<int, IProgrammer>>();
+            expectedQueue.Enqueue(new Tuple<int, IProgrammer>(1, programmer2));
+            expectedQueue.Enqueue(new Tuple<int, IProgrammer>(1, programmer3));
 
             Assert.That(queue, Is.EquivalentTo(expectedQueue));
         }
