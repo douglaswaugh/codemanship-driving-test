c2509a4de6e7febe461db7db7cde5d1ca6623f00
Douglas Waugh <douglas.waugh@energyhelpline.com>
2018-02-21 16:14:16 +0000

    Verify degrees of separation network is built correctly at multiple levels

--- tests/pronet-tests/DegreesOfSeparationNetworkTests.cs
+++ tests/pronet-tests/DegreesOfSeparationNetworkTests.cs
@@ -86,5 +86,130 @@ namespace Tests
 
             Assert.That(queue, Is.EquivalentTo(expectedQueue));
         }
+
+        [Test]
+        public void Should_build_network_with_two_degrees_of_separation()
+        {
+            var programmer1 = BuildProgrammer("Programmer1");
+            var programmer2 = BuildProgrammer("Programmer2");
+            var programmer3 = BuildProgrammer("Programmer3");
+
+            programmer1.Recommends(programmer2);
+            programmer2.Recommends(programmer3);
+
+            var degreesOfSeparation = new DegreesOfSeparationNetwork();
+            var queue = degreesOfSeparation.BuildNetwork(programmer1);
+
+            var expectedQueue = new Queue<Tuple<int, IProgrammer>>();
+            expectedQueue.Enqueue(new Tuple<int, IProgrammer>(1, programmer2));
+            expectedQueue.Enqueue(new Tuple<int, IProgrammer>(2, programmer3));
+
+            Assert.That(queue, Is.EquivalentTo(expectedQueue));
+        }
+
+        [Test]
+        public void Should_build_network_with_three_degrees_of_separation()
+        {
+            var programmer1 = BuildProgrammer("Programmer1");
+            var programmer2 = BuildProgrammer("Programmer2");
+            var programmer3 = BuildProgrammer("Programmer3");
+            var programmer4 = BuildProgrammer("Programmer4");
+
+            programmer1.Recommends(programmer2);
+            programmer2.Recommends(programmer3);
+            programmer3.Recommends(programmer4);
+
+            var degreesOfSeparation = new DegreesOfSeparationNetwork();
+            var queue = degreesOfSeparation.BuildNetwork(programmer1);
+
+            var expectedQueue = new Queue<Tuple<int, IProgrammer>>();
+            expectedQueue.Enqueue(new Tuple<int, IProgrammer>(1, programmer2));
+            expectedQueue.Enqueue(new Tuple<int, IProgrammer>(2, programmer3));
+            expectedQueue.Enqueue(new Tuple<int, IProgrammer>(3, programmer4));
+
+            Assert.That(queue, Is.EquivalentTo(expectedQueue));
+        }
+
+        [Test]
+        public void Should_build_network_with_three_degrees_of_separation_and_recommended_by_connections()
+        {
+            var jill = BuildProgrammer("Jill");
+            var bill = BuildProgrammer("Bill");
+            var ed = BuildProgrammer("Ed");
+            var rick = BuildProgrammer("Rick");
+
+            bill.Recommends(jill);
+            ed.Recommends(bill);
+            ed.Recommends(rick);
+            rick.Recommends(ed);
+
+            var degreesOfSeparation = new DegreesOfSeparationNetwork();
+            var queue = degreesOfSeparation.BuildNetwork(jill);
+
+            var expectedQueue = new Queue<Tuple<int, IProgrammer>>();
+            expectedQueue.Enqueue(new Tuple<int, IProgrammer>(1, bill));
+            expectedQueue.Enqueue(new Tuple<int, IProgrammer>(2, ed));
+            expectedQueue.Enqueue(new Tuple<int, IProgrammer>(3, rick));
+
+            Assert.That(queue, Is.EquivalentTo(expectedQueue));
+        }
+
+        [Test]
+        public void Should_build_network_provided_in_pronet_xml()
+        {
+            var jill = BuildProgrammer("Jill");
+            var bill = BuildProgrammer("Bill");
+            var ed = BuildProgrammer("Ed");
+            var rick = BuildProgrammer("Rick");
+            var nick = BuildProgrammer("Nick");
+            var dave = BuildProgrammer("Dave");
+            var stu = BuildProgrammer("Stu");
+            var frank = BuildProgrammer("Frank");
+            var liz = BuildProgrammer("Liz");
+            var jason = BuildProgrammer("Jason");
+
+            bill.Recommends(jason);
+            bill.Recommends(jill);
+            bill.Recommends(nick);
+            bill.Recommends(stu);
+            dave.Recommends(jill);
+            ed.Recommends(liz);
+            ed.Recommends(rick);
+            ed.Recommends(bill);
+            frank.Recommends(nick);
+            jason.Recommends(dave);
+            jason.Recommends(liz);
+            jill.Recommends(nick);
+            liz.Recommends(bill);
+            nick.Recommends(stu);
+            rick.Recommends(ed);
+            stu.Recommends(frank);
+
+            var degreesOfSeparation = new DegreesOfSeparationNetwork();
+            var queue = degreesOfSeparation.BuildNetwork(jill);
+
+            var expectedQueue = new Queue<Tuple<int, IProgrammer>>();
+            expectedQueue.Enqueue(BuildTuple(1, nick));
+            expectedQueue.Enqueue(BuildTuple(1, dave));
+            expectedQueue.Enqueue(BuildTuple(1, bill));
+            expectedQueue.Enqueue(BuildTuple(2, stu));
+            expectedQueue.Enqueue(BuildTuple(2, frank));
+            expectedQueue.Enqueue(BuildTuple(2, jason));
+            expectedQueue.Enqueue(BuildTuple(2, liz));
+            expectedQueue.Enqueue(BuildTuple(2, ed));
+            expectedQueue.Enqueue(BuildTuple(3, rick));
+
+            Assert.That(queue, Is.EquivalentTo(expectedQueue));
+        }
+
+        private IProgrammer BuildProgrammer(string name)
+        {
+            return new Programmer(name, new string[]{});
+        }
+
+        private Tuple<int, IProgrammer> BuildTuple(int degreeOfSeparation, IProgrammer programmer)
+        {
+            return new Tuple<int, IProgrammer>(degreeOfSeparation, programmer);
+        }
     }
 }
