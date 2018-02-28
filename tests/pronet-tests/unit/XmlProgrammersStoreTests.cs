de47a3074724af7e574488a0ef8cee738fd2405a
Douglas Waugh <douglas.waugh@energyhelpline.com>
2018-02-28 10:39:17 +0000

    Verify xml programmers store builds programmers with correct recommendations

--- tests/pronet-tests/unit/XmlProgrammersStoreTests.cs
+++ tests/pronet-tests/unit/XmlProgrammersStoreTests.cs
@@ -1,4 +1,6 @@
 ﻿using NUnit.Framework;
+using NSubstitute;
+using System.Xml.Linq;
 
 namespace ProNet.Test.Unit
 {
@@ -11,6 +13,34 @@ namespace ProNet.Test.Unit
             var programmersStore = new XmlProgrammersStore(new HardCodedXmlLoader(), new ProgrammersFactory(new ProgrammerFactory()));
             var programmers = programmersStore.GetAll();
             Assert.That(programmers.RankFor("Nick"), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Should_build_programmers_with_correct_recommendations()
+        {
+            var xmlLoader = Substitute.For<IXmlLoader>();
+            xmlLoader
+                .Load()
+                .Returns(XElement.Parse(@"
+                    <?xml version=""1.0"" encoding=""utf-8"" ?>
+                    <Network>
+                        <Programmer name='Nick'></Programmer>
+                        <Programmer name='Bill'></Programmer>
+                        <Programmer name='Dave'></Programmer>
+                        <Programmer name='Ed'>
+                            <Recommendations>
+                                <Recommendation>Liz</Recommendation>
+                                <Recommendation>Rick</Recommendation>
+                                <Recommendation>Bill</Recommendation>
+                            </Recommendations>
+                        </Programmer>
+                        <Programmer name='Liz'></Programmer>
+                        <Programmer name='Rick'></Programmer>
+                    </Network>".Trim()));
+
+            var programmersStore = new XmlProgrammersStore(new HardCodedXmlLoader(), new ProgrammersFactory(new ProgrammerFactory()));
+            var programmers = programmersStore.GetAll();
+            Assert.That(programmers.RecommendationsFor("Ed"), Is.EquivalentTo(new string[] {"Liz", "Rick", "Bill"}));
         }
     }
 }
