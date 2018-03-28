e4090b1a058fbb4228d26afa92df2b2ee896e7df
Douglas Waugh <douglas.waugh@energyhelpline.com>
2018-03-28 12:41:39 +0100

    Extracts method

--- src/pronet/DegreesOfSeparationNetwork.cs
+++ src/pronet/DegreesOfSeparationNetwork.cs
@@ -15,17 +15,19 @@ namespace ProNet
         private List<Tuple<int, IProgrammer>> BuildNetwork(Queue<Tuple<int, IProgrammer>> toProcess, List<Tuple<int, IProgrammer>> network)
         {
             while (toProcess.Count > 0)
-            {
-                var programmerToProcess = toProcess.Dequeue();
-
-                if (!NetworkContainsProgrammer(network, programmerToProcess))
-                {
-                    network.Add(programmerToProcess);
-                    programmerToProcess.Item2.AddRelationsTo(toProcess, programmerToProcess.Item1 + 1);
-                }
-            }
+                AddProgrammerToNetwork(toProcess, network);
 
             return network;
+        }
+
+        private void AddProgrammerToNetwork(Queue<Tuple<int, IProgrammer>> toProcess, List<Tuple<int, IProgrammer>> network)
+        {
+            var programmerToProcess = toProcess.Dequeue();
+            if (!NetworkContainsProgrammer(network, programmerToProcess))
+            {
+                network.Add(programmerToProcess);
+                programmerToProcess.Item2.AddRelationsTo(toProcess, programmerToProcess.Item1 + 1);
+            }
         }
 
         private List<Tuple<int, IProgrammer>> InitialiseNetwork()
