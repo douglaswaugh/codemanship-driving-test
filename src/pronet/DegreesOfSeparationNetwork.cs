f69f93ea604a81e32b89dd3eafe993a28f36f4c5
Douglas Waugh <douglas.waugh@energyhelpline.com>
2018-03-28 12:27:45 +0100

    Inlines private method

--- src/pronet/DegreesOfSeparationNetwork.cs
+++ src/pronet/DegreesOfSeparationNetwork.cs
@@ -21,7 +21,7 @@ namespace ProNet
                 if (!NetworkContainsProgrammer(network, programmerToProcess))
                 {
                     network.Add(programmerToProcess);
-                    AddRelationsTo(programmerToProcess.Item2, toProcess, network, programmerToProcess.Item1 + 1);
+                    programmerToProcess.Item2.AddRelationsTo(toProcess, programmerToProcess.Item1 + 1);
                 }
             }
 
@@ -31,11 +31,6 @@ namespace ProNet
         private bool NetworkContainsProgrammer(List<Tuple<int, IProgrammer>> network, Tuple<int, IProgrammer> programmerToProcess)
         {
             return network.Any(tuple => tuple.Item2.Equals(programmerToProcess.Item2));
-        }
-
-        private void AddRelationsTo(IProgrammer processed, Queue<Tuple<int, IProgrammer>> queue, List<Tuple<int, IProgrammer>> network, int degreeOfSeparation)
-        {
-            processed.AddRelationsTo(queue, degreeOfSeparation);
         }
     }
 }
