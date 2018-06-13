88da14f8d58976524788bc0b7635d0fc35aa6a8e
Douglas Waugh <douglas.waugh@energyhelpline.com>
2018-06-13 12:18:05 +0100

    Extracts team member strength method

--- src/pronet/Team.cs
+++ src/pronet/Team.cs
@@ -20,11 +20,17 @@ namespace ProNet
         public decimal Strength =>
             ((decimal)1 / (decimal)_members.Count()) *
             _members
-                .Select(member =>
-                    new Tuple<decimal,int, int>(
-                        member.Details.Rank,
-                        Array.IndexOf(member.Details.Skills.ToArray(), _language) + 1,
-                        _members.First().Equals(member) ? 1 : _degreesOfSeparation.Between(_members.First(), member)))
-                .Aggregate(0m, (total, rankSkillDegree) => total += rankSkillDegree.Item1 / (rankSkillDegree.Item2 * rankSkillDegree.Item3));
+                .Select(member => MemberStrength(member))
+                .Sum();
+
+        private decimal MemberStrength(IProgrammer member)
+        {
+            var rankSkillDegree = new Tuple<decimal, int, int>(
+                member.Details.Rank,
+                Array.IndexOf(member.Details.Skills.ToArray(), _language) + 1,
+                _members.First().Equals(member) ? 1 : _degreesOfSeparation.Between(_members.First(), member));
+
+            return rankSkillDegree.Item1 / (rankSkillDegree.Item2 * rankSkillDegree.Item3);
+        }
     }
 }
